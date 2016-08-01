e1e0a22ef31ed27f4ca5b07a449f518243cc438e
ibtehalahmed <ibtehal955@gmail.com>
2016-08-01 19:18:31 +0200

    fixed response

--- contacts-app/routes/contact.js
+++ contacts-app/routes/contact.js
@@ -7,34 +7,45 @@ module.exports = function(app) {
             phone: req.body.phone
         });
         newContact.save(function (err) {
-            if (err)return;
+            if (err)res.send("this contact already exists phone should be unique ");
+            else
+            res.send("added successfully");
         });
     });
     app.get('/', function (req, res) {
-        Contact.find(function (err) {
+        Contact.find(function (err,docs) {
             if (err)
-                return;
+                res.send('error');
+            else
+                res.send(docs);
+
         });
     });
     app.delete('/delete/:id', function (req, res) {
         var id = req.params.id;
        Contact.remove({_id: id}, function (err) {
             if (err)
-                return
+                res.send("contact not found")
+           else
+               res.send("deleted successfully")
         });
     });
     app.get('/contact/:id', function (req, res) {
         var id = req.params.id;
-        Contact.findOne({_id: id}, function (err) {
+        Contact.findOne({_id: id}, function (err,doc) {
             if(err)
-                return;
+                res.send("not found");
+            else
+                res.send(doc);
         });
     });
     app.put('/editcontact/:id', function (req, res) {
         var query = { _id: req.params.id };
         Contact.update(query, { name: req.body.name , phone: req.body.phone }, function(err){
             if(err )
-                return;
+                res.send("this phone number already exists , phone should be unique");
+            else
+                res.send("updated successfully")
         });
     });
 };
